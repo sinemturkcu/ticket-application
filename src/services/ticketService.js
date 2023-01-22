abc4b649fd59e67f0362e299f6d24d93b72595f7
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-01-23 02:38:53 +0300

    Update ticketService.js

--- src/services/ticketService.js
+++ src/services/ticketService.js
@@ -4,4 +4,9 @@ export default class ResumeService {
   getAllTickets() {
     return axios.get("http://localhost:8082/api/ticket/getAll");
   }
+  getByFilter(departureCity, destinationCity) {
+    return axios.post(
+      `http://localhost:8082/api/ticket/getByFilter?departureCity=${departureCity}&destinationCity=${destinationCity}`
+    );
+  }
 }
