2d2ad4e47ce1f9e05949af09b033eae9366fb984
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-01-22 23:11:22 +0300

    Update Ticket.js

--- src/pages/Ticket.js
+++ src/pages/Ticket.js
@@ -1,20 +1,16 @@
 import React from "react";
+import HeroSection from "../components/heroSection";
 import Navbar from "../components/navbar";
 import TicketCard from "../components/ticketCard";
+
 class Ticket extends React.Component {
   render() {
     return (
       <div>
         <Navbar />
+        <HeroSection />
 
-        <div className="mt-10 mx-20 flex justify-center grid grid-cols-2 gap-2 mb-2">
-          <div>
-            <TicketCard />
-          </div>
-          <div>
-            <TicketCard />
-          </div>
-        </div>
+        <TicketCard />
       </div>
     );
   }
