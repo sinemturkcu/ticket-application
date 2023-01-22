f1b52153b53a200b251061a8bb0a9abe1d2a03e4
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-01-23 02:38:48 +0300

    Update Ticket.js

--- src/pages/Ticket.js
+++ src/pages/Ticket.js
@@ -1,6 +1,6 @@
 import React from "react";
 import FindRoadCard from "../components/findRoadCard";
-import TicketCard from "../components/ticketCard";
+import TicketCard from "../components/ticketByCityCard";
 import Navbar from "../components/navbar";
 
 class Ticket extends React.Component {
