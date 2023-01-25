7341af33317c837f7cac60d6a92d5fc72399e860
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-01-26 00:53:47 +0300

    setVehicle method added

--- src/services/VehicleService.js
+++ src/services/VehicleService.js
@@ -1,6 +1,10 @@
-export const GetAllVehicles = () => {
-  var request = fetch("http://localhost:8082/api/vehicle/getAll", {
-    method: "GET",
+export const setVehicle = (body) => {
+  var request = fetch("http://localhost:8082/api/vehicle/save", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
   });
 
   return request;
