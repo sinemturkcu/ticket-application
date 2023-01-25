bf19d43299a4cb94a736147401a00212972ef3cb
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-01-25 21:27:50 +0300

    AdminHome and AllUsers pages created

---
+++ src/pages/Admin/AllUsers.js
@@ -0,0 +1,22 @@
+import React from "react";
+import AllUsersTable from "../../components/allUsersTable";
+import Sidebar from "../../components/sidebar";
+
+class SelamAdmin extends React.Component {
+  render() {
+    return (
+      <div>
+        <Sidebar />
+        <div class="sm:ml-64">
+          <h3 className="m-4 p-4 text-xl">All Users</h3>
+          <div class="m-4 p-4 border-dashed border-gray-200 dark:border-gray-700 border-2 rounded-lg">
+            <p class="dark:text-white text-gray-800">
+              <AllUsersTable />
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default SelamAdmin;
