5601ebeae03d17bc33ddb81ef8924e18b7f22316
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-01-26 23:13:04 +0300

    Pages added (empty)

---
+++ src/pages/SelamAdmin.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+class SelamAdmin extends React.Component{
+    render() {
+        return (
+          <>
+            Selam Admin
+          </>
+        );
+      }
+}
+export default SelamAdmin;
