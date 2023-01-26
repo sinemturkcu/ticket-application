bc3463bedbd0400000441f530cad142cbf73bda3
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-01-26 06:59:07 +0300

    getVehicle method added.

--- src/components/allVehicles.js
+++ src/components/allVehicles.js
@@ -1,87 +1,99 @@
-import React, { useState, useEffect } from "react";
-import VehicleService from "../services/AxiosService";
+import React, { useState, useEffect, Component } from "react";
+import { getVehicle } from "../services/VehicleService";
 
-function Vehicles() {
-  const [vehicles, vehicle] = useState([]);
-  let adminService = new VehicleService();
+class Vehicles extends Component {
+  state = {
+    myTickets: [],
+  };
 
-  useEffect(() => {
-    adminService.getAllVehicles().then((result) => vehicle(result.data));
-  });
+  constructor() {
+    super();
+    getVehicle().then((res) =>
+      res.json().then((tickets) => {
+        let p = [...this.state.myTickets];
+        for (let i = 0; i < tickets.length; i++) {
+          p.push(tickets[i]);
+        }
+        this.setState({ myTickets: p });
+      })
+    );
+  }
 
-  return (
-    <div class="flex flex-col">
-      <div class="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
-        <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-          <div class="overflow-hidden">
-            <table class="min-w-full">
-              <thead class="bg-gray-200 border-b">
-                <tr>
-                  <th
-                    scope="col"
-                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                  >
-                    #
-                  </th>
-                  <th
-                    scope="col"
-                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                  >
-                    Capacity
-                  </th>
-                  <th
-                    scope="col"
-                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                  >
-                    Seat Price
-                  </th>
-                  <th
-                    scope="col"
-                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                  >
-                    Departure City
-                  </th>
-                  <th
-                    scope="col"
-                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                  >
-                    Destination City
-                  </th>
-                  <th
-                    scope="col"
-                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                  >
-                    Departure Time
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {vehicles.map((vehicle) => (
-                  <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"></td>
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      {vehicle.capacity}
-                    </td>
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      {vehicle.seatPrice}
-                    </td>
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      {vehicle.departureCity}
-                    </td>
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      {vehicle.destinationCity}
-                    </td>
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      {vehicle.departureTime}
-                    </td>
+  render() {
+    return (
+      <div class="flex flex-col">
+        <div class="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
+          <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="overflow-hidden">
+              <table class="min-w-full">
+                <thead class="bg-gray-200 border-b">
+                  <tr>
+                    <th
+                      scope="col"
+                      class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    >
+                      #
+                    </th>
+                    <th
+                      scope="col"
+                      class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    >
+                      Capacity
+                    </th>
+                    <th
+                      scope="col"
+                      class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    >
+                      Seat Price
+                    </th>
+                    <th
+                      scope="col"
+                      class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    >
+                      Departure City
+                    </th>
+                    <th
+                      scope="col"
+                      class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    >
+                      Destination City
+                    </th>
+                    <th
+                      scope="col"
+                      class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                    >
+                      Departure Time
+                    </th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {this.state.myTickets.map((vehicle) => (
+                    <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"></td>
+                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        {vehicle.capacity}
+                      </td>
+                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        {vehicle.seatPrice}
+                      </td>
+                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        {vehicle.departureCity}
+                      </td>
+                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        {vehicle.destinationCity}
+                      </td>
+                      <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        {vehicle.departureTime}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 export default Vehicles;
--- src/services/VehicleService.js
+++ src/services/VehicleService.js
@@ -1,6 +1,19 @@
+const backendApiUrl = "http://localhost:8082/api";
+
 export const setVehicle = (body) => {
   var request = fetch("http://localhost:8082/api/vehicle/save", {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  return request;
+};
+export const getVehicle = (body) => {
+  var request = fetch(backendApiUrl + "/vehicle/getAll", {
+    method: "GET",
     headers: {
       "Content-Type": "application/json",
     },
