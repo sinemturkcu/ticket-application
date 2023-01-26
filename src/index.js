c0c9944597eb5debbc370ebc15a43b82e2a293a0
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-01-26 08:40:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "flowbite";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
@@ -11,7 +12,14 @@ import Main from "./pages/Main";
 import TicketCard from "./components/ticketCard";
 import Ticket from "./pages/Ticket";
 import AdminHome from "./pages/Admin/AdminHome";
-import "flowbite";
+import AllUsers from "./pages/Admin/AllUsers";
+import AddUsers from "./pages/Admin/AddUsers";
+import DeleteUser from "./pages/Admin/DeleteUser";
+import AllVehicles from "./pages/Admin/AllVehicles";
+import AddVehicle from "./pages/Admin/AddVehicle";
+import MyTickets from "./pages/MyTickets";
+import FindBus from "./pages/FindBus";
+import DeleteVehicle from "./pages/Admin/DeleteVehicle";
 
 <script
   src="https://cdnjs.cloudflare.com/ajax/libs/react-modal/3.14.3/react-modal.min.js"
@@ -31,7 +39,14 @@ class MyElement extends React.Component {
           <Route path="/selamUser" element={<SelamUser />} />
           <Route path="/ticket" element={<Ticket />} />
           <Route path="admin/home" element={<AdminHome />} />
-
+          <Route path="admin/allUsers" element={<AllUsers />} />
+          <Route path="admin/addUser" element={<AddUsers />} />
+          <Route path="admin/allVehicles" element={<AllVehicles />} />
+          <Route path="admin/addVehicle" element={<AddVehicle />} />
+          <Route path="/myTickets" element={<MyTickets />} />
+          <Route path="/findBus" element={<FindBus />} />
+          <Route path="admin/deleteUser" element={<DeleteUser />} />
+          <Route path="admin/deleteVehicle" element={<DeleteVehicle />} />
           <Route path="/" element={<Main />} />
         </Routes>
       </BrowserRouter>
