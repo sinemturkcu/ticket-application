5591274fe9b9923104740d640dec2e57d7a53274
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-01-26 20:12:29 +0300

    Update heroSection.js

--- src/components/heroSection.js
+++ src/components/heroSection.js
@@ -11,7 +11,7 @@ function HeroSection() {
 
         <h4>Travel without limits</h4>
         <p>What are you waiting for?</p>
-        <a href="/ticket">
+        <a href="/ticketSearch">
           <button
             type="button"
             class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-md px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 mt-10"
