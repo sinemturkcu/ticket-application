ea1da29d1d5d4401672dc2dfb2f3f46d43743485
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-01-26 07:09:28 +0300

    deleteVehicleById  method added

--- src/services/VehicleService.js
+++ src/services/VehicleService.js
@@ -11,6 +11,7 @@ export const setVehicle = (body) => {
 
   return request;
 };
+
 export const getVehicle = (body) => {
   var request = fetch(backendApiUrl + "/vehicle/getAll", {
     method: "GET",
@@ -19,6 +20,21 @@ export const getVehicle = (body) => {
     },
     body: JSON.stringify(body),
   });
+
+  return request;
+};
+
+export const deleteVehicleById = (userId) => {
+  var request = fetch(
+    "http://localhost:8082/api/vehicle/delete/?id=" + userId,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(),
+    }
+  );
 
   return request;
 };
