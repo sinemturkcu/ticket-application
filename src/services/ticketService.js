49bc6ec9b19d391e0c2b14fab3dce3d949b87b00
sinemturkcu <42895382+sinemturkcu@users.noreply.github.com>
2023-01-26 23:11:53 +0300

    Authorization header added.

--- src/services/ticketService.js
+++ src/services/ticketService.js
@@ -43,7 +43,6 @@ export const GetByDepartureAndDirections = ({
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        Authorization: "Bearer " + localStorage.getItem("accessToken"),
       },
     }
   );
